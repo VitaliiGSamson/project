190cfa82dc48cc382f1a4479a8bebeb016b692dc
Vitalii <example@gmail.com>
2021-06-11 18:19:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,148 @@
+"use strict";
+
+alert('Hello');
+
+console.log(4 +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;     /////префіксні incr++ постфіксні
+// --decr;                    decr--
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert('Hello');
+
+// const result = confirm("Are you here?"); ////stroka
+// console.log(result);
+
+// // const answer = +prompt("Вам есть 18?", "18");  /////stroka
+// // console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = promt('Как ваше имя?','');
+// answers[1] = promt('Как вашa фамимя?','');
+// answers[2] = promt('Скольок вам лет?','');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// document.write(answers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
